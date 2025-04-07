5f4e048b357bc2ad2d7fc2189785d4973a837ccf
quiambao970 <quiambao970@gmail.com>
2025-04-08 06:56:13 +0800

    add pre title

--- Components/BlockPageContent/functions.php
+++ Components/BlockPageContent/functions.php
@@ -3,6 +3,7 @@
 namespace Flynt\Components\BlockPageContent;
 
 add_filter('Flynt/addComponentData?name=BlockPageContent', function (array $data): array {
+    $data['pre_title'] = get_field('pre_title', $data['post']->ID);
     $data['hero_background_image'] = get_field('hero_background_image', $data['post']->ID);
     $data['hero_content'] = get_field('hero_content', $data['post']->ID);
 
